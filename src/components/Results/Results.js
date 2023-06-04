7c9232740772dbd18f45270597203dc4c35fd09f
Muhammad Andika Fadilla <46752973+dekrayz@users.noreply.github.com>
2023-06-04 18:01:25 +0700

    Update Results.js

--- src/components/Results/Results.js
+++ src/components/Results/Results.js
@@ -21,7 +21,7 @@ const Results = ({ results, processing }) => {
               <div className="results__wrapper" key={i}>
                 <div style={{ width: '300px' }}>
                   <p>
-                    saya rasa salah satu dari kalian sedang...{mapExpressionToEmosi(result.expressions.asSortedArray()[0].expression)}
+                    saya rasa salah satu dari kalian adalah seorang {mapExpressionToEmoji(results[0].gender)} dan sedang mapExpressionToEmosi(result.expressions.asSortedArray()[0].expression)}
                   </p>
                 </div>
                 <FontAwesomeIcon icon={mapExpressionToEmoji(result.expressions.asSortedArray()[0].expression)} size="2x" />
@@ -31,7 +31,7 @@ const Results = ({ results, processing }) => {
         ) : (
           <div className="results__wrapper">
             <div>
-              <p>saya rasa anda sedang...{mapExpressionToEmosi(results[0].expressions.asSortedArray()[0].expression)}</p>
+              <p>dari hasil analisa saya, anda adalah seorang {mapExpressionToEmoji(results[0].gender)} dan saya rasa emosi anda sekarang adalah mapExpressionToEmosi(results[0].expressions.asSortedArray()[0].expression)}</p>
             </div>
             <div className="results__emoji">
               <FontAwesomeIcon icon={mapExpressionToEmoji(results[0].expressions.asSortedArray()[0].expression)} size="2x" />
